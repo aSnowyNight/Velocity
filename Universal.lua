0e2a96b5e8c0f4c72650d2459db04c243d49aa75
aSnowyNight <97135872+aSnowyNight@users.noreply.github.com>
2024-06-26 14:08:15 -0700

    Update Universal.lua

--- Universal.lua
+++ Universal.lua
@@ -41,6 +41,7 @@ do
         Title = "Button",
         Description = "Very important button",
         Callback = function()
+                when clicked ru
             Window:Dialog({
                 Title = "Title",
                 Content = "This is a dialog",
